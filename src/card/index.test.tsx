131ddf88863c13926df59a3f8e53c69f0defccff
Omer Prizner <omer.prizner@gmail.com>
2019-11-29 17:05:54 +0200

    minor cards test twick

--- src/card/index.test.tsx
+++ src/card/index.test.tsx
@@ -5,11 +5,10 @@ import { ECardSuit } from './constant/suit';
 import { ECardValue } from './constant/value';
 
 it('renders without crashing', () => {
-  render(<Card suit={ECardSuit.HEARTS} value={1} />);
+  render(<Card suit={ECardSuit.HEARTS} value={ECardValue.KING} />);
 });
 
 it('renders card, kings of heart', async () => {
   const { findByTestId } = render(<Card suit={ECardSuit.HEARTS} value={ECardValue.KING} />);
   await findByTestId(`card-img-${ECardSuit.HEARTS}_${ECardValue.KING}`);
 });
-
